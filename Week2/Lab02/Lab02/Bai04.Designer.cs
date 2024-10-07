580e5380d5290df926c0a02c2b0c19b6e0880f56
Phuctt205dev <tonyde2005@gmail.com>
2024-10-07 11:05:23 +0700

    Repaired Lab02

--- Week2/Lab02/Lab02/Bai04.Designer.cs
+++ Week2/Lab02/Lab02/Bai04.Designer.cs
@@ -28,19 +28,97 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnBack = new System.Windows.Forms.Button();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.lbPage = new System.Windows.Forms.Label();
+            this.rtb1 = new System.Windows.Forms.RichTextBox();
+            this.rtb2 = new System.Windows.Forms.RichTextBox();
+            this.rtb3 = new System.Windows.Forms.RichTextBox();
             this.SuspendLayout();
+            // 
+            // btnBack
+            // 
+            this.btnBack.Location = new System.Drawing.Point(502, 384);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(75, 23);
+            this.btnBack.TabIndex = 0;
+            this.btnBack.Text = "BACK";
+            this.btnBack.UseVisualStyleBackColor = true;
+            this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
+            // 
+            // btnNext
+            // 
+            this.btnNext.Location = new System.Drawing.Point(603, 384);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(75, 23);
+            this.btnNext.TabIndex = 1;
+            this.btnNext.Text = "NEXT";
+            this.btnNext.UseVisualStyleBackColor = true;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // lbPage
+            // 
+            this.lbPage.AutoSize = true;
+            this.lbPage.Location = new System.Drawing.Point(583, 387);
+            this.lbPage.Name = "lbPage";
+            this.lbPage.Size = new System.Drawing.Size(14, 16);
+            this.lbPage.TabIndex = 2;
+            this.lbPage.Text = "1";
+            // 
+            // rtb1
+            // 
+            this.rtb1.Location = new System.Drawing.Point(261, 12);
+            this.rtb1.Name = "rtb1";
+            this.rtb1.ReadOnly = true;
+            this.rtb1.Size = new System.Drawing.Size(173, 395);
+            this.rtb1.TabIndex = 3;
+            this.rtb1.Text = "";
+            // 
+            // rtb2
+            // 
+            this.rtb2.Location = new System.Drawing.Point(261, 12);
+            this.rtb2.Name = "rtb2";
+            this.rtb2.ReadOnly = true;
+            this.rtb2.Size = new System.Drawing.Size(173, 395);
+            this.rtb2.TabIndex = 4;
+            this.rtb2.Text = "";
+            // 
+            // rtb3
+            // 
+            this.rtb3.Location = new System.Drawing.Point(261, 12);
+            this.rtb3.Name = "rtb3";
+            this.rtb3.ReadOnly = true;
+            this.rtb3.Size = new System.Drawing.Size(173, 395);
+            this.rtb3.TabIndex = 5;
+            this.rtb3.Text = "";
+            this.rtb3.TextChanged += new System.EventHandler(this.rtb3_TextChanged);
             // 
             // Bai04
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1446, 896);
+            this.ClientSize = new System.Drawing.Size(691, 419);
+            this.Controls.Add(this.rtb3);
+            this.Controls.Add(this.rtb2);
+            this.Controls.Add(this.rtb1);
+            this.Controls.Add(this.lbPage);
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.btnBack);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Bai04";
             this.Text = "Bai04";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnBack;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.Label lbPage;
+        private System.Windows.Forms.RichTextBox rtb1;
+        private System.Windows.Forms.RichTextBox rtb2;
+        private System.Windows.Forms.RichTextBox rtb3;
     }
 }
