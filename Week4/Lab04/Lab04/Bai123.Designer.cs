928c13437c2f5e66457e78907bca4262bb85104f
PhNgii <johndoe@example.com>
2024-11-01 23:02:05 +0700

    xong b123 lab4

--- Week4/Lab04/Lab04/Bai123.Designer.cs
+++ Week4/Lab04/Lab04/Bai123.Designer.cs
@@ -1,5 +1,6 @@
 ﻿namespace Lab04
 {
+    using HtmlAgilityPack;
     partial class Bai123
     {
         /// <summary>
@@ -19,7 +20,7 @@
             }
             base.Dispose(disposing);
         }
-
+        
         #region Windows Form Designer generated code
 
         /// <summary>
@@ -28,12 +29,168 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.url = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.fileurl = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // url
+            // 
+            this.url.Location = new System.Drawing.Point(78, 12);
+            this.url.Multiline = true;
+            this.url.Name = "url";
+            this.url.Size = new System.Drawing.Size(518, 30);
+            this.url.TabIndex = 0;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 186);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(320, 252);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // fileurl
+            // 
+            this.fileurl.Location = new System.Drawing.Point(78, 77);
+            this.fileurl.Multiline = true;
+            this.fileurl.Name = "fileurl";
+            this.fileurl.Size = new System.Drawing.Size(518, 30);
+            this.fileurl.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "URL";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(8, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "FILE URL";
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(325, 113);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1438, 560);
+            this.panel1.TabIndex = 6;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(611, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(91, 28);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "GET";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(708, 11);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(80, 30);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Download";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(611, 71);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(91, 34);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "AddBrowser";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(708, 71);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(80, 34);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "GO";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(30, 129);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(139, 23);
+            this.button5.TabIndex = 11;
+            this.button5.Text = "DownloadResource";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(187, 129);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(132, 23);
+            this.button6.TabIndex = 12;
+            this.button6.Text = "DownloadFile";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // Bai123
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.fileurl);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.url);
+            this.Name = "Bai123";
             this.Text = "Bai123";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox url;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox fileurl;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
     }
 }
--- Week4/Lab04/Lab04/Bai123.cs
+++ Week4/Lab04/Lab04/Bai123.cs
@@ -1,20 +1,159 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System.Net;
 using System.Windows.Forms;
+using System.Linq; // Thêm namespace để sử dụng Any()
+using HtmlAgilityPack; // Đảm bảo HtmlAgilityPack được khai báo đúng
 
 namespace Lab04
 {
+    using HtmlAgilityPack; // Đảm bảo HtmlAgilityPack được khai báo đúng
+
     public partial class Bai123 : Form
     {
         public Bai123()
         {
             InitializeComponent();
+        }
+
+        WebBrowser wb;
+
+        private string GetHTML(string url)
+        {
+            try
+            {
+                WebRequest request = WebRequest.Create(url);
+                using (WebResponse response = request.GetResponse())
+                using (Stream dataStream = response.GetResponseStream())
+                using (StreamReader reader = new StreamReader(dataStream))
+                {
+                    return reader.ReadToEnd();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error fetching HTML: " + ex.Message);
+                return string.Empty;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string urlText = url.Text.ToString();
+            richTextBox1.Text = GetHTML(urlText);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string urlText = url.Text.ToString();
+            string filePath = fileurl.Text.ToString();
+            richTextBox1.Text = GetHTML(urlText);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            wb = new WebBrowser();
+            wb.Width = 1920;
+            wb.Height = 1080;
+            wb.ScriptErrorsSuppressed = true;
+            panel1.Controls.Add(wb);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            string html = wb.DocumentText;
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            saveFileDialog1.Filter = "Html files (*.html)|*.html|All files (*.*)|*.*";
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                using (StreamWriter sw = new StreamWriter(saveFileDialog1.OpenFile()))
+                {
+                    sw.Write(html);
+                }
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string link = url.Text;
+            wb.Navigate(link);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            string urlText = url.Text.ToString();
+            if (string.IsNullOrEmpty(urlText))
+            {
+                MessageBox.Show("Please enter a URL.");
+                return;
+            }
+
+            using (FolderBrowserDialog folderDialog = new FolderBrowserDialog())
+            {
+                if (folderDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string saveFolder = folderDialog.SelectedPath;
+                    try
+                    {
+                        DownloadImages(urlText, saveFolder);
+                        MessageBox.Show("Images downloaded successfully to " + saveFolder);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Error downloading images: " + ex.Message);
+                    }
+                }
+            }
+        }
+
+        private void DownloadImages(string url, string saveFolder)
+        {
+            string htmlContent = GetHTML(url);
+            if (string.IsNullOrEmpty(htmlContent))
+            {
+                MessageBox.Show("Failed to fetch HTML content.");
+                return;
+            }
+
+            HtmlDocument doc = new HtmlDocument();
+            doc.LoadHtml(htmlContent);
+
+            // Lấy tất cả các thẻ <img> và thuộc tính src của chúng
+            var imageNodes = doc.DocumentNode.SelectNodes("//img[@src]");
+            if (imageNodes == null || imageNodes.Count == 0) // Kiểm tra Count thay vì Any()
+            {
+                MessageBox.Show("No images found on this page.");
+                return;
+            }
+
+            foreach (var imgNode in imageNodes)
+            {
+                string imgSrc = imgNode.GetAttributeValue("src", null);
+                if (!string.IsNullOrEmpty(imgSrc))
+                {
+                    // Xử lý đường dẫn tuyệt đối và tương đối
+                    Uri imgUri = new Uri(new Uri(url), imgSrc);
+                    DownloadImage(imgUri.ToString(), saveFolder);
+                }
+            }
+        }
+
+        private void DownloadImage(string imageUrl, string saveFolder)
+        {
+            try
+            {
+                string fileName = Path.GetFileName(new Uri(imageUrl).LocalPath);
+                string savePath = Path.Combine(saveFolder, fileName);
+
+                using (WebClient client = new WebClient())
+                {
+                    client.DownloadFile(imageUrl, savePath);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error downloading image " + imageUrl + ": " + ex.Message);
+            }
         }
     }
 }
