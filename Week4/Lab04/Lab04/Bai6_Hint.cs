0b6b16f0f3ee1f21df16c2b6889cb00fcaa503b7
Phuctt205dev <tonyde2005@gmail.com>
2024-11-02 22:56:22 +0700

    Add themes for Bai6

---
+++ Week4/Lab04/Lab04/Bai6_Hint.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab04
+{
+    public partial class Bai6_Hint : Form
+    {
+        public Bai6_Hint()
+        {
+            InitializeComponent();
+        }
+    }
+}
