36002d0ee08d2c9ccc6f0b06ea171e0a11a34811
Nam Thanh <90046176+ThanhNam23@users.noreply.github.com>
2024-11-14 08:23:47 +0700

    dashboard

---
+++ Week5/Lab05/Lab05/Dashboard.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab05
+{
+    public partial class Dashboard : Form
+    {
+        public Dashboard()
+        {
+            InitializeComponent();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Bai03 button3 = new Bai03();
+            button3.ShowDialog();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Bai01 button1 = new Bai01();
+            button1.ShowDialog();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Bai02 button2 = new Bai02();
+            button2.ShowDialog();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Bai04 button4 = new Bai04();
+            button4.ShowDialog();
+        }
+    }
+}
